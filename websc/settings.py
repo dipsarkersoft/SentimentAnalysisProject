22d704508794ab5ee3198e1c9a5e7e4c363ef96c
dip_sarker <sarkerdip005@gmail.com>
2025-05-03 17:30:00 +0600

    CSRF_TRUSTED_ORIGINS add

--- websc/settings.py
+++ websc/settings.py
@@ -18,7 +18,13 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 
+CORS_ALLOW_ALL_ORIGINS = True
 
+CORS_ALLOWED_ORIGINS = [
+
+    "http://127.0.0.1:5173",  # Alternate localhost
+]
+CSRF_TRUSTED_ORIGINS = ['https://sentimentanalysisproject-koak.onrender.com/']
 # Application definition
 
 INSTALLED_APPS = [
